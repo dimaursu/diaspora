5694f43e2d6b91c6244d51b19b927394e495833b
Lukas Matt <lukas@zauberstuhl.de>
2014-08-01 08:03:13 -0400

    Added database tables for xmpp server
    To fully support external clients it should be
    also possible to save non-diaspora contacts

---
+++ db/migrate/20140801101230_create_chat_contacts.rb
@@ -0,0 +1,17 @@
+class CreateChatContacts < ActiveRecord::Migration
+  def up
+    create_table :chat_contacts do |t|
+      t.integer :user_id, limit: 11, null: false
+      t.string :jid, null: false
+      t.string :name, limit: 255, null: true
+      t.string :ask, limit: 128, null: true
+      t.string :subscription, limit: 128, null: false
+      t.text :groups, null: true
+    end
+    add_index :chat_contacts, [:user_id, :jid], unique: true
+  end
+
+  def down
+    drop_table :chat_contacts
+  end
+end
