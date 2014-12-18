4ebf4087ad20dfbafc2dfbc9d047ab81d6fc7fa5
Dumitru Ursu <dima@ceata.org>
2014-12-18 16:32:41 +0200

    Remove mobile toggle

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -7,13 +7,6 @@ class HomeController < ApplicationController
     partial_dir = Rails.root.join('app', 'views', 'home')
     if user_signed_in?
       redirect_to stream_path
-    elsif is_mobile_device?
-      if partial_dir.join('_show.mobile.haml').exist? ||
-         partial_dir.join('_show.mobile.erb').exist?
-        render :show
-      else
-        redirect_to user_session_path
-      end
     elsif partial_dir.join("_show.html.haml").exist? ||
           partial_dir.join("_show.html.erb").exist?
       render :show, layout: 'application'
@@ -21,17 +14,5 @@ class HomeController < ApplicationController
       render file: Rails.root.join("public", "default.html"),
              layout: 'application'
     end
-  end
-
-  def toggle_mobile
-    if session[:mobile_view].nil?
-      # we're most probably not on mobile, but user wants it anyway
-      session[:mobile_view] = true
-    else
-      # switch from mobile to normal html
-      session[:mobile_view] = !session[:mobile_view]
-    end
-
-    redirect_to :back
   end
 end
