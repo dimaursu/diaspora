dc4f5c072da67cdbd3b2451b3ab5f70fac0ad89b
Dumitru Ursu <dima@ceata.org>
2015-01-19 21:26:58 +0200

    Add properly place the login form

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150114094206) do
+ActiveRecord::Schema.define(version: 20150119173412) do
 
   create_table "account_deletions", force: true do |t|
     t.string   "diaspora_handle"
